92e91539534275fca5bd9d7d6f44cc12ae73d961
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-19 18:46:14 +0100

    Fixed bug in input

--- script.js
+++ script.js
@@ -75,16 +75,15 @@ console.log(print);
 
 // * Chiedo all'utente il nome il cognome e l'età 
 
-/*
-const newName = prompt('Inserisci il tuo nome').trim();
-const newSurname = prompt('Inserisci il tuo cognome').trim();
-const newAge = parseInt(prompt('Inserisci la tua età'));
-if(isNaN(newAge)) {
-   newAge = parseInt(prompt('Inserisci la tua età'));
-}
-*/
+// const newName = prompt('Inserisci il tuo nome').trim();
+// const newSurname = prompt('Inserisci il tuo cognome').trim();
+// const newAge = parseInt(prompt('Inserisci la tua età'));
+// if(isNaN(newAge)) {
+//    newAge = parseInt(prompt('Inserisci la tua età'));
+// }
 
-classes.push({ name:`${newName}`, cognome: `${newSurname}`, eta: newAge});
+
+// classes.push({ name:`${newName}`, cognome: `${newSurname}`, eta: newAge});
 
 
 // ! ----------------- BUNUS ---------------------
@@ -104,6 +103,6 @@ generate.addEventListener('click', function(){
    paragraph.className = 'clicked';
    write.appendChild(paragraph);
 
-   paragraph.innerText = `il Sign. ${inputUserName} ${inputUserSurname}  di età ${inputUserEta} è pregato di spostare l'auto `;
+   paragraph.innerText = `il Sign ${inputUserName} ${inputUserSurname}  di età ${inputUserEta} è pregato di spostare l'auto `;
 
 });
